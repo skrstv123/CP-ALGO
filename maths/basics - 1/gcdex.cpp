c8254d2fd7051835e949c7a2a2804877a28c35ba
skrstv123 <skrstv123@gmail.com>
2019-10-17 00:30:05 +0530

    added basics 1

---
+++ maths/basics - 1/gcdex.cpp
@@ -0,0 +1,102 @@
+/*1
+Extended Euclidean algorithm
+
+gcd(a,b) can be written as 
+a.x + b.y = gcd(a,b)
+
+Extended Euclidean algorithm also finds integer coefficients x and y
+
+The extended Euclidean algorithm updates results of gcd(a, b) using the results calculated by recursive call gcd(b%a, a). Let values of x and y calculated by the recursive call be x1 and y1. x and y are updated using the below expressions.
+
+
+x = y1 - b//a * x1
+y = x1
+ 
+*/
+#include<iostream.h>
+
+
+
+int d,x,y;
+void gcdex( int a, int b)
+{
+	if(!b){
+		d=a;
+		x=1;
+		y=0;
+	}
+	else{
+		gcdex(b,  a%b);
+		int temp= x;
+		x=y;
+		y=temp - (a/b)*y; 
+	}
+	
+}
+
+/*2
+This algorithm is used when A and B are co-prime.
+ In such cases, x becomes the multiplicative modulo inverse of A under modulo B, and y becomes the multiplicative modulo inverse of B under modulo A.
+ This has been explained in detail in the Modular multiplicative inverse section.
+*/
+
+
+//int main()
+//{
+	//gcdex(45,48);
+	//cout<< "gcd value is "<<d<<"\n";
+	//cout<< "x, y = "<<x<<" " <<y;
+//}
+
+//Modular multiplicative inverse
+// (a.b)%m = 1 // b is mul mod inv of a mod m 
+//it exists iff gcd(a,m)=1
+// so from gcdex a.x + m.y = 1 and somehow it is believed that mulmodinv b = x 
+
+int d,x,y;
+int mulmodinv(int A, int M)
+{
+    gcdex(A,M);
+    return (x%M+M)%M;    //x may be negative
+}
+
+/*3
+mulmodinv when p is a prime 
+
+This approach uses Fermat's Little Theorem.
+
+a^(m-1) ≡ 1 (mod m)  
+If we multiply both sides with a-1, we get
+
+ a^-1 ≡ a^( m-2) (mod m)   //explain how
+ */
+int gcd(int A, int B) {
+    if(B==0)
+        return A;
+    else
+        return GCD(B, A % B);
+}
+int modexp(int b, int ex, int mod)
+{
+int res=1;
+while(ex)
+{
+	if(ex&1)
+		res=(res*b)%mod;
+	b=(b*b)%mod;
+	ex>>=1;
+}
+return res;	
+}
+
+void mulmodinv(int a, int m) 
+{ 
+    int g = gcd(a, m); 
+    if (g != 1) 
+        cout << "Inverse doesn't exist"; 
+    else
+    {
+        cout << "Modular multiplicative inverse is "
+             << modexp(a, m-2, m); 
+    } 
+} 
---
+++ maths/basics - 1/modular.py
@@ -0,0 +1,61 @@
+#modular arithmetic
+"""
+When one number is divided by another, the modulo operation finds the remainder. It is denoted by the % symbol.
+properties;
+	1> (a+b)%c  =  (a%c + b%c)%c 
+	2> (a*b)%c = ((a%c)*(b%c))%c
+	3> (a-b)%c = ((a%c) - (b%c) + c)%c
+	4> (a/b)%c = ((a%c)*(b^-1%c))%c
+	
+"""
+#modular exponentiation
+# x^n
+#1 recursive
+def binaryExponentiation( x, n):
+    if(n==0):
+		return 1
+    elif(n%2 == 0):        #n is even
+        return binaryExponentiation(x*x,n/2)
+    else:                             #n is odd
+        return x*binaryExponentiation(x*x,(n-1)/2)
+
+#2 iterative
+def binaryExponentiation(x,n):
+    result=1
+    while(n>0):
+    
+        if(n % 2 ==1):
+            result=result * x
+        x=x*x
+        n=n/2
+    return result  
+	
+#3 modular exponentiation recursive
+def modularExponentiation(x,n,M):
+	"""
+	Time complexity: O(log N)
+	Memory complexity: O(log N) because a function call consumes memory and log N recursive function calls are made
+
+	"""
+    if(n==0)
+        return 1
+    elif(n%2 == 0):        #n is even
+        return modularExponentiation((x*x)%M,n/2,M)
+    else:                             #n is odd
+        return (x*modularExponentiation((x*x)%M,(n-1)/2,M))%M
+
+#4 modularExponentiation iterative
+def binaryExponentiation(x,n,M):
+	"""
+	Time complexity: O(log N)
+	Memory complexity: O(1)
+	"""
+    result=1
+    while(n>0):
+    
+        if(n&1):
+            result=(result * x)%M
+        x=(x*x)%M
+        n>>=2
+    return result  
+	
