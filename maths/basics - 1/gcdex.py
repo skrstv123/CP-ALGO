72a771c97764a9f1ac7684eca4cdb14e351a742e
skrstv123 <47166055+skrstv123@users.noreply.github.com>
2019-11-26 09:34:00 +0530

    Create gcdex.py

---
+++ maths/basics - 1/gcdex.py
@@ -0,0 +1,22 @@
+def gcdExtended(a, b, x=1, y=1): 
+
+    if a == 0 :  
+        x = 0
+        y = 1
+
+        return b 
+
+    x1 = 1
+    y1 = 1
+
+    gcd = gcdExtended(b%a, a, x1, y1)
+
+    x = y1 - (b/a) * x1 
+    y = x1 
+
+    print(x,y) 
+
+    return gcd 
+
+print(gcdExtended(3,12)) 
+#print(x,y)
