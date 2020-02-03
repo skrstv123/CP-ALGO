541f8bc24cd6f6df6bb66895f755d0b8991c7dc1
skrstv123 <skrstv123@gmail.com>
2020-02-04 02:55:14 +0530

    basics2

---
+++ maths/basics2/ndigits.py
@@ -0,0 +1,15 @@
+"""
+to print the nth digit consisting of digits {0,1,2,3,4,5,6}
+
+Ans :(for general case) n in base len(set of given digits) 
+"""
+def toBase(n,b):
+    if n==0: return 0 
+    l=[]
+    while n:
+        l.append(str(n%b))
+        n//=b 
+    return ''.join(l[::-1])
+
+N = int(input()) 
+print(toBase(N,6))
---
+++ maths/basics2/ndigits2.py
@@ -0,0 +1,32 @@
+'''
+given a number (int)'abc..' {{ALL DIGITS MUST BE UNIQUE!}}
+find the next greater number with the same set of digits
+'''
+#iterate from right towards left; find first number which isnt in ascending order
+#if found none: number itself is the max
+#else : find smallest from its right side and swap them to get the answer
+
+from bisect import bisect as bs
+
+def findNext(n):
+    #print(534976,536479)
+    sn = list(map(int,str(n)))[::-1]
+    #print(sn)
+    for i in range(len(str(sn))-1):
+        
+        if not sn[i]<sn[1+i]:
+            nl = sn[:i+1] ; p = sn[i+1]
+            #print(nl,p)
+                        
+            q,nl[bs(nl,p)] = nl[bs(nl,p)],p 
+            nl.append(q) 
+            #print(nl)
+            
+            nl[:-1] = sorted(nl[:-1],reverse=1) #for further optimization, here also we may use bs as the list is already sorted
+            #print(nl)
+                     
+            gl = nl + sn[i+2:] 
+            return ''.join(list(map(str,gl))[::-1])
+    return -1 
+    
+print(findNext(66))
