c5ac1de2df71ee9297d666a3a75cabee7ea5939e
Shekhar Srivastava <47166055+skrstv123@users.noreply.github.com>
2021-02-25 00:32:51 +0530

    Update buildroads.py

--- dsa/cses/graph/buildroads.py
+++ dsa/cses/graph/buildroads.py
@@ -23,18 +23,3 @@ for i in range(1,n+1):
 print(len(l)-1)
 for i in range(len(l)-1):
     print(l[i],l[i+1])
-"""
-Anubhav Singh
-1:16 AM
-https://www.youtube.com/watch?v=mnIlh6__q6w
-Anubhav Singh
-1:18 AM
-https://github.com/xprilion/trustedforms-docs
-Aniket Kumar
-1:23 AM
-http://www.ianiket.codes/trustedforms-docs/
-
-Abhishek Nandy
-1:41 AM
-redis-cli -h 104.131.70.242 -a tfredis123
-"""
